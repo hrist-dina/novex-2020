7638812d0085bd2f88402b811f505e2a42392c37
Dina Hrist <hdina@inbox.ru>
2020-11-28 15:37:42 +0700

    Added desktop

--- src/js/classes/Menu.js
+++ src/js/classes/Menu.js
@@ -78,8 +78,19 @@ export class Menu {
     }
 
     setActiveLink(link) {
-        if ( link ) {
+        if (link) {
             link.closest('.menu__item').addClass('active').siblings().removeClass('active');
+
+            const menuBtn = $('.js-menu-panel');
+            const href = link.attr('href');
+            if (!menuBtn.length) {
+                return;
+            }
+            if (href === '#prizes') {
+                menuBtn.addClass('colored');
+            } else {
+                menuBtn.removeClass('colored');
+            }
         }
     }
 }
